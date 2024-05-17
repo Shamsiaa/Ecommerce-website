70560ad0b00841474742438e055d9bd56c9b1822
ciara1940 <ciaracheah.2021@scis.smu.edu.sg>
2024-05-17 02:08:01 +0200

    added routing to product detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,11 @@ export const routes: Routes = [
         path: 'register',
         component: RegisterComponent,
     },
+    {
+        path: 'products/:id',
+        component: ProductDetailComponent
+     },
+     
     {
         path: '',
         component: ProductListComponent,
--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../services/product';
+import { ActivatedRoute } from '@angular/router'
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+  id!: string;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+  }
   @Input() product!: Product;
 }
