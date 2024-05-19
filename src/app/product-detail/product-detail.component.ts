cacd09443d3a843e28a90203ca2a82df79816935
ciara1940 <ciaracheah.2021@scis.smu.edu.sg>
2024-05-19 22:23:22 +0200

    updated product details

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { Product } from '../services/product';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-detail',
+  imports: [CommonModule],
   standalone: true,
-  imports: [],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product!: Product;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const productId = params.get('id');
+      if (productId) {
+        this.productService.getProductMetadata(productId).subscribe(product => {
+          this.product = product;
+          console.log(product)
+        }, error => {
+          console.error(error);
+        });
+      }
+    });
+  }
 }
